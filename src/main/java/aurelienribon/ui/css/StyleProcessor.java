7c73c1622cd437462ee00fce9f03eec65d30ef0e
KokaKiwi <admin@kokabsolu.com>
2012-04-27 20:30:36 +0200

    Moved "Java Universal CSS Engine" to FengGUI

--- src/main/java/aurelienribon/ui/css/StyleProcessor.java
+++
@@ -1,9 +0,0 @@
-package aurelienribon.ui.css;
-
-/**
- * @author Aurelien Ribon | http://www.aurelienribon.com/
- */
-public interface StyleProcessor
-{
-    public void process(Object target, StyleRuleSet rules);
-}
