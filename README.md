7a77feac8c5a16cb9b0cbce9149f7676816fe5d5
KokaKiwi <admin@kokabsolu.com>
2012-05-23 01:30:14 +0200

    - Added license informations/headers - Added javadoc and source JARs generation - Added README - Updated libraries

---
+++ README.md
@@ -0,0 +1,29 @@
+Kowy Maker - Specifications
+===========================
+
+Informations
+------------
+
+**_Kowy Maker - Specifications_** is a Java library for **_Kowy Maker Project_** which contains all dependencies shared between project's parts.
+
+Package
+-------
+
+**_Kowy Maker - Specifications_** contains:
+
+- jdom: XML DOM Library [2.0.1] - http://www.jdom.org/
+- snakeyaml: YAML parser and emitter for Java [1.10] - http://www.snakeyaml.org
+- Netty: asynchronous event-driven network application framework [3.4.5 Final] - http://netty.io
+- Apache Commons IO [2.3] - http://commons.apache.org/io/
+- Guava: Google Core Libraries for Java 1.6+ [12.0] - http://code.google.com/p/guava-libraries/
+- Apache Commons Math [3.0] - http://commons.apache.org/math/
+- Apache Commons Logging [1.1.1] - http://commons.apache.org/logging/
+- FastPNG PNGDecoder - http://sourceforge.net/projects/fastpng/
+- Universal Tween Engine (embed) [6.3.1] - http://code.google.com/p/java-universal-tween-engine/
+
+Others parts
+------------
+
+- Client: https://github.com/KowyMaker/Client
+- Server: https://github.com/KowyMaker/Server
+- Editor: https://github.com/KowyMaker/Editor
--- src/main/java/com/kowymaker/spec/utils/Configuration.java
+++ src/main/java/com/kowymaker/spec/utils/Configuration.java
@@ -1,5 +1,21 @@
 /**
  * This file is part of Kowy Maker.
+ *
+ * Kowy Maker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kowy Maker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kowy Maker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is part of Kowy Maker.
  * 
  * Kowy Maker is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -26,9 +42,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import org.yaml.snakeyaml.Yaml;
 
 public class Configuration
